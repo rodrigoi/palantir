9e20785a87b21b4a8f7639139b299edb9f15245d
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2014-01-21 02:03:41 -0300

    Added config variables for sauron.

--- app.js
+++ app.js
@@ -105,7 +105,9 @@ app.get("/logout", function(req, res){
 });
 
 app.get("/video", ensureAuthenticated, function(req, res) {
-  request.get("http://home.gandalfelgris.com.ar:8080/videostream.cgi?user=&pwd=").pipe(res);
+  var user = process.env.SAURON_USER || "";
+  var pwd = process.env.SAURON_PWD || "";
+  request.get("http://home.gandalfelgris.com.ar:8080/videostream.cgi?user=" + user + "&pwd=" + pwd).pipe(res);
 });
 
 var port = process.env.PORT || 3000;
